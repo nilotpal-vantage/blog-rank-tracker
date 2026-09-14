71c96eabd3b0060cf9deb160b54fbcb1ccb8d17f
Nilotpal Saharia <nilotpal.saharia@vantagecircle.com>
2026-09-14 10:04:21 +0530

    Auto-refresh data (2026-09-14)

--- data.js
+++ data.js
@@ -1,12 +1,11 @@
 // Auto-generated. Do not edit.
 const META = {
-  "history_start": "2025-09-08",
-  "history_end": "2026-09-06",
+  "history_start": "2025-09-15",
+  "history_end": "2026-09-13",
   "history_days": 364,
-  "first_week": "2025-W37",
-  "last_week": "2026-W36",
+  "first_week": "2025-W38",
+  "last_week": "2026-W37",
   "all_weeks": [
-    "2025-W37",
     "2025-W38",
     "2025-W39",
     "2025-W40",
@@ -57,8 +56,9 @@ const META = {
     "2026-W33",
     "2026-W34",
     "2026-W35",
-    "2026-W36"
+    "2026-W36",
+    "2026-W37"
   ],
-  "generated_at": "2026-09-07T04:34:24.242238Z",
+  "generated_at": "2026-09-14T04:34:20.952917Z",
   "post_count": 67
 };
